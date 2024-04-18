a3c6fb3bf288147ff1265fef6eb8a376b1d6d642
MD Naem <mmnayeem408@gmail.com>
2024-04-18 12:46:30 +0600

    form add

--- app/enroll/page.js
+++ app/enroll/page.js
@@ -1,11 +1,12 @@
 import Banner from "@/components/Banner";
+import Form from "@/components/Form";
 
 
 
 export default function Home() {
   return (
     <main className="text-gray-300 container w-[90%]  mx-auto">
-      <Banner/>
+      <Form></Form>
       
       
     </main>
--- components/Navber.js
+++ components/Navber.js
@@ -19,7 +19,7 @@ export default function Navber() {
                     <div className="hidden w-full md:block md:w-auto" id="navbar-default">
                         <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
                             <li>
-                                <a href="#" className="block py-2 px-3  rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-indigo-500 md:p-0 ">About</a>
+                                <a href="enroll" className="block py-2 px-3  rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-indigo-500 md:p-0 ">About</a>
                             </li>
 
                         </ul>
